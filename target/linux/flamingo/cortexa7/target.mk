f2ac47be2aebabdc65cb4993c4a786315e97493a
Jinlong Huang <447845069@qq.com>
2023-02-17 09:49:36 +0800

    Add flamingo board

---
+++ target/linux/flamingo/cortexa7/target.mk
@@ -0,0 +1,7 @@
+BOARDNAME:=Flamingo with i.MX Cortex-A7
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
+
+define Target/Description
+	Build firmware images for Automic  Flamingo(i.MX Cortex-A7) boards.
+endef
---
+++ target/linux/flamingo/cortexa9/target.mk
@@ -0,0 +1,8 @@
+
+BOARDNAME:=Flamingo with i.MX Cortex-A9
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=neon
+
+define Target/Description
+	Build firmware images for Automic  Flamingo(i.MX Cortex-A9) boards.
+endef
